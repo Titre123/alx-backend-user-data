4e863f209dcddd244da6b7759f08f05ba49318db
Taiwo <taiwotriumphant@gmail.com>
2023-02-03 00:03:45 +0100

    type annotatted

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -10,18 +10,8 @@ import re
 
 
 def filter_datum(fields: List[str], redaction: str, message: str,
-                 separator: str):
-    '''
-        Args:
-            - fields: a list of strings representing all fields to
-            obfuscate
-            - redaction: a string representing by what the field will be
-            obfuscated
-            - message: a string representing the log line
-            - seperator: a string representing by which character is
-            separating
-            all fields in the log line (message)
-    '''
+                 separator: str) -> str:
+    ''' Filters a log line '''
     new_message = [re.sub(_[re.search(_.split('=')[0], _).span()[1] + 1:],
                    redaction, _)if _.split('=')[0] in fields else _ for _ in
                    message.split(separator)]
