e6253bb2a3c27602717a939ef7856b2849ed7e51
Taiwo <taiwotriumphant@gmail.com>
2023-02-08 12:46:53 +0100

    basic authentification class

---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+""" Basic Authentication module for the API.
+"""
+
+
+from typing import List, TypeVar
+from flask import request
+from api.v1.auth.auth import Auth
+
+
+class BasicAuth(Auth):
+    """
+    """
+    pass
