57a13fcb7dab91a973bab1a3d1e895e574ed994c
Taiwo <taiwotriumphant@gmail.com>
2023-02-08 01:36:20 +0100

    done with question 5

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -34,7 +34,9 @@ class Auth:
             Return:
                 None
         """
-        return None
+        if request is None or 'Authorization' not in request.headers:
+            return None
+        return request.headers.get('Authorization')
 
     def current_user(self, request=None) -> TypeVar('User'):
         """
