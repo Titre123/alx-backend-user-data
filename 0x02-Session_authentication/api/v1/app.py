3ac59d4a3c66c96a1ec97437075cc1dbab0272f5
Taiwo <taiwotriumphant@gmail.com>
2023-02-10 00:25:32 +0100

    session auth

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -9,6 +9,7 @@ from flask_cors import (CORS, cross_origin)
 import os
 from api.v1.auth.auth import Auth
 from api.v1.auth.basic_auth import BasicAuth
+from api.v1.auth.session_auth import SessionAuth
 
 
 app = Flask(__name__)
@@ -23,6 +24,8 @@ if auth_type == 'auth':
 elif auth_type == "basic_auth":
     authen = BasicAuth()
     auth = authen
+elif auth_type = "SessionAuth":
+    auth = SessionAuth()
 
 
 @app.before_request
---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+""" Session Authentication module for the API.
+"""
+
+
+from api.v1.auth.auth import Auth
+import uuid
+
+
+class SessionAuth(Auth):
+    """
+        Session Autnentification class
+    """
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """
+            Create session
+        """
+        if user_id is None or type(user_id) != str:
+            return None
+        else:
+            session_id = str(uuid.uuid4())
+            SessionAuth.user_id_by_session_id[session_id] = user_id
+            return session_id
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """
+            get user_id
+        """
+        if session_id is None or type(session_id) != str:
+            return None
+        return SessionAuth.user_id_by_session_id.get(session_id)
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        '''
+            get current user
+        '''
+        session_id = self.session_cookie(request)
+        user_id = self.user_id_for_session_id(session_id)
+        return user_id
